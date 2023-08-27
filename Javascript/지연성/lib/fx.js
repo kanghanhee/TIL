b428280bd5d6daefb9cca0a9a3f420109da539c2
kanghanhee <kanghanhee59@gmail.com>
2023-08-27 19:13:22 +0900

    느긋하고 영리한  지연평가

--- Javascript/지연성/lib/fx.js
+++ Javascript/지연성/lib/fx.js
@@ -5,32 +5,32 @@ const curry =
   (a, ..._) =>
     _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
-const map = curry((f, iter) => {
-  let res = [];
-  for (const a of iter) {
-    res.push(f(a));
-  }
-  return res;
-});
+// const map = curry((f, iter) => {
+//   let res = [];
+//   for (const a of iter) {
+//     res.push(f(a));
+//   }
+//   return res;
+// });
 
-const filter = curry((f, iter) => {
-  let res = [];
-  for (const a of iter) {
-    if (f(a)) res.push(a);
-  }
-  return res;
-});
+// const filter = curry((f, iter) => {
+//   let res = [];
+//   for (const a of iter) {
+//     if (f(a)) res.push(a);
+//   }
+//   return res;
+// });
 
-const reduce = curry((f, acc, iter) => {
-  if (!iter) {
-    iter = acc[Symbol.iterator]();
-    acc = iter.next().value;
-  }
-  for (const a of iter) {
-    acc = f(acc, a);
-  }
-  return acc;
-});
+// const reduce = curry((f, acc, iter) => {
+//   if (!iter) {
+//     iter = acc[Symbol.iterator]();
+//     acc = iter.next().value;
+//   }
+//   for (const a of iter) {
+//     acc = f(acc, a);
+//   }
+//   return acc;
+// });
 
 const go = (...args) => reduce((a, f) => f(a), args);
 
